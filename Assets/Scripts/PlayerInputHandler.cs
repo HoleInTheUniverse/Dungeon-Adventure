0c6ac5f86c760225d1e74741d885d2a7effc86e7
HoleInTheUniverse <zahv96@hmail.com>
2018-10-22 16:15:05 +0300

    Add combat
    Added basic attack and started adding weapons

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -6,16 +6,21 @@ public class PlayerInputHandler : MonoBehaviour
 {
     private bool jump;
     private PlayerMovement player;
+    private WeaponBehaviour weapon;
 
     private void Awake()
     {
         player = GetComponent<PlayerMovement>();
+        weapon = transform.Find("Weapon").GetComponentInChildren<WeaponBehaviour>();
     }
 
     private void Update()
     {
         if (!jump)
             jump = Input.GetButtonDown("Jump");
+
+        if (Input.GetButtonDown("Fire1"))
+            weapon.Attack();
     }
 
     private void FixedUpdate()
---
+++ Assets/Scripts/Weapons/WeaponBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    private Animator weaponAnimator;
+
+    private void Start()
+    {
+        weaponAnimator = GetComponent<Animator>();
+    }
+
+    public void Attack()
+    {
+        weaponAnimator.SetTrigger("Attack");
+    }
+}
